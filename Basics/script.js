fc63abd740d06aabc6d4ae20baa1e345859ed141
lokesh190496 <lokeshvasu63@gmail.com>
2025-02-24 11:46:06 +0530

    variables

--- Basics/script.js
+++ Basics/script.js
@@ -1,1 +1,4 @@
 console.log("welcome to javascript")
+// alert("welcome to our website")
+console.log(typeof(alert))
+console.log(window)
---
+++ Variables/variables.js
@@ -0,0 +1,11 @@
+// let x = 100;
+// let y = 200;
+// let z;
+// z = x + y;
+// console.log("z", z);
+
+let name = "Lokesh";
+console.log(typeof name);
+age = 28;
+
+console.log(`${name} age is ${age}`);
