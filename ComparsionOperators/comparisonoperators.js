1c182d62dd40154896c6d8aaedf30318dee27533
lokesh190496 <lokeshvasu63@gmail.com>
2025-02-26 12:49:33 +0530

    comparision operators

---
+++ ComparsionOperators/comparisonoperators.js
@@ -0,0 +1,44 @@
+// console.log("Crocodile" > "Parrot")
+// console.log("A" > "Z")
+// console.log("Suresh" > "Surya")
+
+// console.log("crocodile" > "Parrot")
+// console.log("a" > "Z")
+// console.log("suresh" > "Surya")
+
+// console.log(1<4)
+// console.log(1>5)
+// console.log(5<5)
+// console.log(5==5)
+// console.log(5<=4)
+// console.log(4>=1)
+// console.log(5<=5)
+// console.log(10!=8)
+// console.log(6==6)
+
+
+// console.log("1" < 4);
+// console.log(10 < false);
+// console.log("hi" == true);
+// console.log("1" == true);
+// console.log("1" == null);
+// console.log(1 > "suresh");
+// console.log("1" < undefined);
+
+
+// console.log("143" == 143)
+// console.log("lokesh" == "lokesh")
+// // console.log("lokesh" == "Lokesh")
+// console.log("0.0" == 0)
+
+// console.log("143" === 143)
+// console.log("suresh" === "suresh")
+// console.log("0.0" === 0)
+
+// console.log(0==null)
+// console.log(null >0)
+// console.log(null >= 0)
+
+console.log(null == undefined)
+console.log(undefined == null)
+console.log(null === undefined)
