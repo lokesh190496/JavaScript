1e5fe34bade7e57d36bf38fbc83534d3999d75da
lokesh190496 <lokeshvasu63@gmail.com>
2025-02-27 09:27:13 +0530

    statements

---
+++ ConditionalStatements/statements.js
@@ -0,0 +1,45 @@
+// let result = prompt("movie is good");
+
+// if (result == "yes") {
+//   console.log("watch movie");
+// } else {
+//   console.log("Don't watch movie");
+// }
+
+// let totalScore = prompt("What is your total score?");
+
+// if (totalScore == 25) {
+//   console.log("Excellent");
+// }
+
+// if (totalScore >= 20) {
+//   console.log("Good");
+// }
+
+// if (totalScore <= 20) {
+//   console.log("Average");
+// }
+
+// if (totalScore == 25) {
+//   console.log("Excellent");
+// }
+// else if(totalScore >= 20){
+//     console.log("Good")
+// }
+// else{
+//     console.log("Average")
+// }
+
+let isSubscribed = prompt("Have you subscribed, type yes otherwise no");
+
+// if(isSubscribed == "yes"){
+//     console.log("Thank you")
+//     console.log("Thank you so much for subscribing")
+// }else{
+//     console.log("please subscribe")
+// }
+
+// isSubscribed == "yes" ? console.log("Thank you") : console.log("please subscribe")
+isSubscribed == "yes"
+  ? (console.log("Thank you"), console.log("Thank you much for subscribing"))
+  : console.log("please subscribe");
