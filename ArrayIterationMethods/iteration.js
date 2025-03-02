f962beaa6cb6c82363341d506ea34d4a5b734eb5
lokesh190496 <lokeshvasu63@gmail.com>
2025-03-02 10:43:31 +0530

    iteration methods

---
+++ ArrayIterationMethods/iteration.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.map((num)=>num*2)
+console.log(result)
+
+const numbers123 = [1, 2, 3, 4, 5];
+numbers123.forEach((num) => console.log(num * 2)); 
